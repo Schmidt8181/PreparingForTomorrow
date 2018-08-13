7cc899eac94def710851520767b496d843ff3d19
Sarah Schmidt <s.a.schmidt.8181@gmail.com>
2018-08-13 11:52:54 -0500

    Put graphs side by side

--- dash_practice.py
+++ dash_practice.py
@@ -4,10 +4,11 @@ import dash_html_components as html
 import pandas as pd
 
 df = pd.read_csv('Food_price_indices_data_jul.csv')
+After2005 = df[192:]
 app = dash.Dash()
 
 app.layout = html.Div(children=[
-    html.H1(children='Holy crap I\'m making a webpage!',
+    html.H1(children='WOW I\'m making a webpage!',
         style={'text-align': 'center',
                 'color': 'rgb(199,0,57)',
                 'background-color': 'rgb(62,60,60)'
@@ -17,18 +18,30 @@ app.layout = html.Div(children=[
     children='''
         Dash: A web application framework for Python.
     '''),
-    dcc.Graph(
-        id='MeatSugar-graph',
-        figure={
-            'data': [
-                {'x': df.Date, 'y': df['Meat Price Index'], 'type': 'line', 'name': 'Meat Price Index'},
-                {'x': df.Date, 'y': df['Sugar Price Index'], 'type': 'line', 'name': 'Sugar Price Index'}
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization WOO!'
-            }
-        }
-    )
+    html.Div(children=[
+        dcc.Graph(id='MeatSugar-graph',
+            figure={
+                'data': [
+                    {'x': After2005.Date, 'y': After2005['Meat Price Index'], 'type': 'line', 'name': 'Meat Price Index'},
+                    {'x': After2005.Date, 'y': After2005['Sugar Price Index'], 'type': 'line', 'name': 'Sugar Price Index'}
+                    ],
+                'layout': {'title': 'Dash Data Visualization WOO!'}
+                    },
+            style={'width': '600', 'display': 'inline-block'}),
+        dcc.Graph(id='DairyOilsCereals-graph',
+            figure={
+                'data': [
+                    {'x': After2005.Date, 'y': After2005['Dairy Price Index'],
+                    'type': 'line', 'name': 'Dairy Price Index'},
+                    {'x': After2005.Date, 'y': After2005['Oils Price Index'],
+                    'type': 'line', 'name': 'Oils Price Index'},
+                    {'x': After2005.Date, 'y': After2005['Cereals Price Index'],
+                    'type': 'line', 'name': 'Cereals Price Index'},
+                    ],
+                'layout': {'title': 'Dash Data Visualization 2 WOO WOO!'}
+                    },
+            style={'width': '600', 'display': 'inline-block'}),
+        ], style={'display': 'inline-block'})
 ])
 
 if __name__ == '__main__':
