ec9db077cce0e06e0218f8672df49e9a6ffb97fb
Sarah Schmidt <s.a.schmidt.8181@gmail.com>
2018-09-21 12:18:54 -0500

    made cleaning unit

---
+++ cleaning/cleaning.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import datetime
+
+def clean_temperature_data(df):
+    temp_df = df.drop(['J-D', 'D-N', 'DJF', 'MAM', 'JJA', 'SON'], axis=1)
+    clean_temp_df = temp_df[110:139].copy()
+    clean_temp_df.rename(columns={'Jan':'1','Feb':'2', 'Mar':'3', 'Apr':'4', 'May':'5', 'Jun':'6', 'Jul':'7', 'Aug':'8', 'Sep':'9', 'Oct':'10', 'Nov':'11', 'Dec':'12'}, inplace=True)
+    clean_temp_df = pd.melt(clean_temp_df, id_vars='Year', value_vars=['1', '2', '3','4','5','6','7','8','9','10','11','12'])
+    clean_temp_df.rename(columns={'variable': 'Month'}, inplace=True)
+    clean_temp_df['Day'] = 1
+    clean_temp_df['Date'] = np.nan
+    clean_temp_df['Date'] = pd.to_datetime(clean_temp_df[['Year','Month', 'Day']])
+    clean_temp_df['Temperature'] = pd.to_numeric(clean_temp_df['value'], errors='coerce')
+    clean_temp_df = clean_temp_df.drop(['Month', 'Day', 'Year', 'value'], axis=1)
+    clean_temp_df.sort_values('Date', inplace=True)
+    clean_temp_df = clean_temp_df.reset_index(drop=True)
+    print(clean_temp_df.tail(7))
+    return clean_temp_df
+
+def clean_food_data(df):
+    df['Date'] = pd.to_datetime(df['Date'])
+    food_data = df.copy()
+    food_data = food_data.drop(['Food Price Index'], axis=1)
+    print(food_data.tail(12))
+    return food_data
